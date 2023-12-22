3e3176ee1f8d8fa705229e3e52d1e96295085538
example <ulgeronur@outlook.com>
2023-12-22 20:44:45 +0300

    update

--- src/SureSayaciThreadOrnegi.java
+++ src/SureSayaciThreadOrnegi.java
@@ -1,5 +1,25 @@
-public class Main {
+public class SureSayaciThreadOrnegi {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Runnable sure = () -> {
+            for (int i = 60; i>=0; i--) {
+                System.out.println("Kalan Süreniz : " + i);
+                try {
+                    Thread.sleep(1000);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread thread1 = new Thread(sure);
+
+        thread1.start();
+
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Süreniz sona erdi!");
     }
 }
